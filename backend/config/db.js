15c5e31063955ed2a33a6ead5a2a2bcd9810b1c4
Dimas <dimasardykusuma01@gmail.com>
2024-06-16 18:50:00 +0700

    Perbarui6

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,17 @@
+require('dotenv').config(); // Memuat variabel lingkungan dari file .env
 const mysql = require("mysql2/promise");
+
 // MySQL database connection
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "database_ils",
-  port: 3308,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
 });
 
 db.getConnection().then((connection) => {
   console.log("Connected to MySQL database");
 });
 
-module.exports = db
+module.exports = db;
